98f1d1db76f82ce9a704a833f04e3e9ebee76b3c
Pulu <Pulu@DESKTOP-HK4MEES>
2017-01-25 19:18:28 +0200

    Vielä kesken..

--- OOP/Tehtävä_19/Program.cs
+++ OOP/Tehtävä_19/Program.cs
@@ -10,6 +10,45 @@ namespace Tehtävä_19
     {
         static void Main(string[] args)
         {
+            List<string> arvatut = new List<string>();
+            string sana = "kakka";
+            int arvauksia = 5;
+            while (arvauksia >= 0)
+            {
+                TulostaArvatut(arvatut);
+                Console.WriteLine("Arvaa sana");
+                string arvaus = Console.ReadLine();
+                arvatut.Add(arvaus);
+                if (arvaus == sana)
+                {
+                    Console.WriteLine("Oikein arvattu");
+                    break;
+                }
+                else
+                {
+                    TarkistaSanasta(arvaus, sana);
+                }
+            }
+
+        }
+        static void TarkistaSanasta(string arvaus, string sana)
+        {
+            for (int i = 0; i < sana.Length; i++)
+            {
+                if (arvaus[i] == sana[i])
+                {
+
+                }
+            }
+        }
+
+        static void TulostaArvatut(List<string> arvatut)
+        {
+            foreach (string arv in arvatut)
+            {
+                Console.WriteLine(" Arvattu jo:" + arv);
+            }
         }
     }
 }
+
