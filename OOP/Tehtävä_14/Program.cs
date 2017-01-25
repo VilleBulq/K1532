c08c1bf4fd09905b56ec0371a69db0f6e4f4da17
Pulu <Pulu@DESKTOP-HK4MEES>
2017-01-25 21:59:54 +0200

    Vielä kesken, muuten ok mutta tulostusta ei ole.

--- OOP/Tehtävä_14/Program.cs
+++ OOP/Tehtävä_14/Program.cs
@@ -10,6 +10,46 @@ namespace Tehtävä_14
     {
         static void Main(string[] args)
         {
+        bool jekku = true;
+            int[] luvut = new int[4];
+        while(jekku) {
+        // Paina 8 niin lopettaa 
+                int arvosana = Int32.Parse(Console.ReadLine());
+                if (arvosana > 5)
+                {
+                    Console.WriteLine("Anna arvosana 1-5");
+                    break;
+                }
+                else if (arvosana == 1)
+                {
+                    luvut[0] += 1;
+                }
+                else if (arvosana == 2)
+                {
+                    luvut[1] += 1;
+                }
+                else if (arvosana == 3)
+                {
+                    luvut[2] += 1;
+                }
+                else if (arvosana == 4)
+                {
+                    luvut[3] += 1;
+                }
+                else if (arvosana == 5)
+                {
+                    luvut[4] += 1;
+                }
+                else if (arvosana == 0)
+                {
+                    jekku = false;
+                }
+                
+                }
+            for(int i =0; i < luvut.Length; i++) {
+                Console.WriteLine("Arvosanoja " + (i+1) + " On " + luvut[i]);
+            }
+            }
         }
     }
-}
+
