ba3c0f9db7c83542b96ca5ab68d2bd5f0f894671
Pulu <Pulu@DESKTOP-HK4MEES>
2017-01-25 23:00:28 +0200

    Muokattu nyt tulostus kuntoon.

--- OOP/Tehtävä_14/Program.cs
+++ OOP/Tehtävä_14/Program.cs
@@ -10,46 +10,56 @@ namespace Tehtävä_14
     {
         static void Main(string[] args)
         {
-        bool jekku = true;
-            int[] luvut = new int[4];
-        while(jekku) {
-        // Paina 8 niin lopettaa 
+            bool jekku = true;
+            int[] luvut = new int[6];
+            while (jekku)
+            {
+                // Paina 8 niin lopettaa 
                 int arvosana = Int32.Parse(Console.ReadLine());
                 if (arvosana > 5)
                 {
-                    Console.WriteLine("Anna arvosana 1-5");
-                    break;
+                    jekku = false;
                 }
                 else if (arvosana == 1)
                 {
-                    luvut[0] += 1;
+                    luvut[1] += 1;
                 }
                 else if (arvosana == 2)
                 {
-                    luvut[1] += 1;
+                    luvut[2] += 1;
                 }
                 else if (arvosana == 3)
                 {
-                    luvut[2] += 1;
+                    luvut[3] += 1;
                 }
                 else if (arvosana == 4)
                 {
-                    luvut[3] += 1;
+                    luvut[4] += 1;
                 }
                 else if (arvosana == 5)
                 {
-                    luvut[4] += 1;
+                    luvut[5] += 1;
                 }
                 else if (arvosana == 0)
                 {
-                    jekku = false;
+                    luvut[0] += 1;
                 }
-                
-                }
-            for(int i =0; i < luvut.Length; i++) {
-                Console.WriteLine("Arvosanoja " + (i+1) + " On " + luvut[i]);
+
             }
-            }
+            String nollia = new string('*', luvut[0]);
+            String ykkosia = new string('*', luvut[1]);
+            String kakkosia = new string('*', luvut[2]);
+            String kolmosia = new string('*', luvut[3]);
+            String nelosia = new string('*', luvut[4]);
+            String vitosia = new string('*', luvut[5]);
+            Console.WriteLine("0: "+ nollia);
+            Console.WriteLine("1: " + ykkosia);
+            Console.WriteLine("2: " + kakkosia);
+            Console.WriteLine("3: " + kolmosia);
+            Console.WriteLine("4: " + nelosia);
+            Console.WriteLine("5: " + vitosia);
+
         }
     }
+}
 
