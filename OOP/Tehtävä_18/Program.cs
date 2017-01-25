5a96245e1cc340c8c38c7f28366a63ece2897503
Pulu <Pulu@DESKTOP-HK4MEES>
2017-01-25 23:06:01 +0200

    Uusi version

--- OOP/Tehtävä_18/Program.cs
+++ OOP/Tehtävä_18/Program.cs
@@ -10,30 +10,53 @@ namespace Tehtävä_18
     {
         static void Main(string[] args)
         {
-        // KAIKKIEN RIVIEN OLTAVA OK ETTÄ ON PALIDROMI :DD 
-        // TÄÄ on aikamoinen viritys
-            Console.WriteLine("Anna sana");
-            string lause = Console.ReadLine();
-            int pituus = lause.Length;
-            Console.WriteLine("Pituus on " + pituus);
-            char[] uusi = new char[pituus];
-            for (int i = 0; i < lause.Length; i++)
+            // KAIKKIEN RIVIEN OLTAVA OK ETTÄ ON PALIDROMI :DD 
+            // TÄÄ on aikamoinen viritys
+            /*
+                Console.WriteLine("Anna sana");
+                string lause = Console.ReadLine();
+                int pituus = lause.Length;
+                Console.WriteLine("Pituus on " + pituus);
+                char[] uusi = new char[pituus];
+                for (int i = 0; i < lause.Length; i++)
+                {
+                    uusi[pituus - 1] = lause[i];
+                    pituus -= 1;
+                }
+                for (int j = 0; j < lause.Length; j++)
+                {
+                    if (lause[j] == uusi[j])
+                    {
+                        Console.WriteLine("On palidromi");
+                    }
+                    else
+                    {
+                        Console.WriteLine("ei ole palidromi");
+                    }
+                }
+            }*/
+            string sana;
+            bool pal = true;
+
+            Console.Write("Anna sana ");
+            sana = Console.ReadLine();
+
+            for (int i = 0; i < sana.Length / 2; i++)
             {
-                uusi[pituus - 1] = lause[i];
-                pituus -= 1;
+                if (sana[i] != sana[sana.Length - 1 - i])
+                {
+                    Console.WriteLine("ei ole palindromi");
+                    pal = false;
+                    break;
+                }
+
             }
-            for (int j = 0; j < lause.Length; j++)
+            if (pal == true)
             {
-                if (lause[j] == uusi[j])
-                {
-                    Console.WriteLine("On palidromi");
-                }
-                else
-                {
-                    Console.WriteLine("ei ole palidromi");
-                }
+                Console.WriteLine("palindromi");
             }
         }
-
     }
 }
+
+
