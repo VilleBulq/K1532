ea29d9104d777ea246b15ca7a250de2267438377
Pulu <Pulu@DESKTOP-HK4MEES>
2017-01-25 20:07:10 +0200

    Tehtävät 5 Valmis OK

--- OOP/Tehtävä_5/Program.cs
+++ OOP/Tehtävä_5/Program.cs
@@ -8,8 +8,17 @@ namespace Tehtävä_5
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+            static void Main(string[] args)
+            {
+                int seconds;
+                seconds = Int32.Parse(Console.ReadLine());
+                int hours = (seconds / 3600);
+                int minutes = (seconds % 3600) / 60;
+                int sekunnit = seconds % 60;
+                Console.WriteLine("Tunnint :" + hours);
+                Console.WriteLine("Minuutit :" + minutes);
+                Console.WriteLine("Sekunnit :" + sekunnit);
+
+            }
         }
     }
-}
