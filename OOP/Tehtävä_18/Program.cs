ce1a1f12bc130fa262b8e40f296ed73104bfc719
Pulu <Pulu@DESKTOP-HK4MEES>
2017-01-25 19:17:21 +0200

    Vielä vähän vaiheessa :D

--- OOP/Tehtävä_18/Program.cs
+++ OOP/Tehtävä_18/Program.cs
@@ -10,6 +10,28 @@ namespace Tehtävä_18
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Anna sana");
+            string lause = Console.ReadLine();
+            int pituus = lause.Length;
+            Console.WriteLine("Pituus on " + pituus);
+            char[] uusi = new char[pituus];
+            for (int i = 0; i < lause.Length; i++)
+            {
+                uusi[pituus - 1] = lause[i];
+                pituus -= 1;
+            }
+            for (int j = 0; j < lause.Length; j++)
+            {
+                if (lause[j] == uusi[j])
+                {
+                    Console.WriteLine("On palidromi");
+                }
+                else
+                {
+                    Console.WriteLine("ei ole palidromi");
+                }
+            }
         }
+
     }
 }
