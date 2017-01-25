8b8bd308bc0acda781cea9a3763ca55d4ac75ce5
Pulu <Pulu@DESKTOP-HK4MEES>
2017-01-25 19:16:41 +0200

    EI vielä toimi

--- OOP/Tehtävä_17/Program.cs
+++ OOP/Tehtävä_17/Program.cs
@@ -10,6 +10,24 @@ namespace Tehtävä_17
     {
         static void Main(string[] args)
         {
+            int[] luvut = { 4, 2, 3, 4, 5 };
+            int[] luvut2 = { 5, 4, 5, 6, 7 };
+            int[] jarjes = new int[9];
+            for (int i = 0; i < 5; i++)
+            {
+                jarjes[i] = luvut[i];
+            }
+            for (int i = 6; i < 9; i++)
+            {
+                jarjes[i] = luvut2[i];
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine(jarjes[i]);
+            }
         }
+
+
+
     }
 }
