00741b9c9049f549b2defd0daeec290a3771697f
Pulu <Pulu@DESKTOP-HK4MEES>
2017-01-31 17:51:22 +0200

    toimiiko

---
+++ Olio-assignments/Olio-assignments/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //WM
+        s
+        }
+    }
+}
